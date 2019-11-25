35b0ae068bf75f31ef05fb40b6e43b2ed74c5b43
unknown <kamila0990@gmail.com>
2019-11-25 22:22:33 +0000

    change all

---
+++ js/proBar.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('.html').animate({
+    width: "80%"
+  }, 3000);
+  $('.bootstrap').animate({
+    width: "80%"
+  }, 3000);
+  $('.js').animate({
+    width: "60%"
+  }, 3000);
+  $('.sass').animate({
+    width: "60%"
+  }, 3000);
+  $('.react').animate({
+    width: "50%"
+  }, 3000);
+})
