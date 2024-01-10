fcc0ac3affdcce46a0161f999a6e5a4204ace0bf
Kamila Dynysiuk <kamila.dynysiuk@gmail.com>
2024-01-10 13:04:32 +0100

    add new icon to social media

--- src/components/common/SocialMedia/index.tsx
+++ src/components/common/SocialMedia/index.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react'
-import { Box, IconButton, useTheme } from '@mui/material'
+import { Box, IconButton, useTheme, Tooltip } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import EmailIcon from '@mui/icons-material/Email'
+import LanguageIcon from '@mui/icons-material/Language'
 import { openInNewTab } from 'utils'
 
 interface IProps {
@@ -11,14 +12,22 @@ interface IProps {
 
 const socialMedia = [
   {
+    name: 'my website',
+    icon: <LanguageIcon color={'inherit'} />,
+    link: 'https://kamcode.pl/',
+  },
+  {
+    name: 'github',
     icon: <GitHubIcon color={'inherit'} />,
     link: 'https://github.com/KamilaDyn',
   },
   {
+    name: 'linkedin',
     icon: <LinkedInIcon color={'inherit'} />,
     link: 'https://www.linkedin.com/in/kamila-d-489849180/',
   },
   {
+    name: 'email',
     icon: <EmailIcon color={'inherit'} />,
     link: 'mailto:kamila.dynysiuk@gmil.com',
   },
@@ -29,22 +38,23 @@ const SocialMedia: FC<IProps> = ({ size }) => {
 
   return (
     <Box display="flex">
-      {socialMedia.map(({ icon, link }, index) => (
-        <IconButton
-          key={index}
-          size={size}
-          sx={{
-            border: `1px solid ${
-              palette.mode === 'light'
-                ? palette.primary.main
-                : palette.primary.contrastText
-            }`,
-            marginRight: 1,
-          }}
-          onClick={() => openInNewTab(link)}
-        >
-          {icon}
-        </IconButton>
+      {socialMedia.map(({ icon, link, name }, index) => (
+        <Tooltip title={name} key={index}>
+          <IconButton
+            size={size}
+            sx={{
+              border: `1px solid ${
+                palette.mode === 'light'
+                  ? palette.primary.main
+                  : palette.primary.contrastText
+              }`,
+              marginRight: 1,
+            }}
+            onClick={() => openInNewTab(link)}
+          >
+            {icon}
+          </IconButton>
+        </Tooltip>
       ))}
     </Box>
   )
